1c6f33157c7e7e2ddd6649427bd40ec44898ce79
syamkumary <syamyala7@gmail.com>
2025-08-17 14:38:43 +0530

    backend changes

---
+++ 02-backend.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#!/bin/bash
+LOGS_FOLDER="/var/log/backend"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+echo "UserID is $USERID"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+if [ $USERID -ne 0 ]
+then
+   echo -e "$R Please run the script with root privileges $N" | tee -a $LOG_FILE
+   exit 1
+fi
+
+}
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is $R Failed $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is $G Success $N" | tee -a $LOG_FILE
+    fi        
+}
+
+echo "Scripts started executing at : $(date)" | tee -a $LOG_FILE
+CHECK_ROOT
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling nodejs"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
+
+useradd expense
+VALIDATE $? "Creating user expense"
